fbb84e1fccb40b9b593b47a28d4176c841cb5e98
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2017-03-19 23:36:30 -0400

    Milestone 2 pre-release

---
+++ 144_ms2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+
+// Define statements:
+
+
+
+
+
+// Constant TAX value:
+
+
+
+
+// Item structure:
+
+
+
+
+
+// Function Prototypes Milestone 1:
+
+
+
+
+
+
+// Function Prototypes Milesone 2:
+
+
+
+
+
+
+
+
+
+// Function Implementations Milestone 1:
+
+
+
+
+
+
+
+
+
+
+
+
+// Function Implementations Milestone 2:
+
+
+
